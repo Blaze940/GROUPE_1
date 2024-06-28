d061cd7153e65c92d1f813566faea14efff9a1d7
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2024-06-28 16:15:45 +0200

    add karcher tests

--- domain/src/test/java/fr/esgi/groupe1/KarcherTest.java
+++ domain/src/test/java/fr/esgi/groupe1/KarcherTest.java
@@ -1,5 +1,6 @@
 package fr.esgi.groupe1;
 
+import fr.esgi.groupe1.exception.NoRecipientToClean;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class KarcherTest {
 
     @Test
-    void cleanRecipient() {
+    void should_clean_recipient_when_not_null() {
+        // Given
+        Karcher karcher = Karcher.getINSTANCE();
+        Recipient recipient = new Recipient(0.5, new Food(true, "water"), new RecipientId());
+
+        // When
+        Recipient cleanedRecipient = karcher.cleanRecipient(recipient);
+
+        // Then
+        assertTrue(cleanedRecipient.isCleaned());
+        assertEquals(0, cleanedRecipient.getCurrentLevel());
+    }
+
+    @Test
+    void should_throw_exception_when_recipient_is_null() {
+        // Given
+        Karcher karcher = Karcher.getINSTANCE();
+
+        // When
+        Exception exception = assertThrows(NoRecipientToClean.class, () -> karcher.cleanRecipient(null));
+
+        // Then
+        assertNotNull(exception);
     }
 }
